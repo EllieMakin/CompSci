2fcc2889ab7a5e6b034490a157646daa4cbb5466
Ellie Makin <eliotmakin@gmail.com>
2019-11-11 23:54:54 +0000

    Finished y2019p1q1

--- Foundations/test.ml
+++ Foundations/test.ml
@@ -1,35 +1,63 @@
-let swapRefs xr yr =
-    let zr = ref !xr
-    in
-        xr := !yr;
-        yr := !zr
+type peano =
+    | Z
+    | S of peano
 ;;
 
-let identityM n =
-    Array.init n (fun row ->
-        Array.init n (fun col ->
-            if col = row then 1 else 0
-        )
-    )
+type binary =
+    bool list
 ;;
 
-let transpose m =
-    let tempR = ref (Array.get (Array.get m 0) 0)
-    and jRow = ref 0
-    and jCol = ref 1
+type 'a church =
+    | X of 'a
+    | F of (unit -> 'a church)
+;;
+
+let rec intFromPeano p =
+    match p with
+    | Z -> 0
+    | S (pn) -> 1 + intFromPeano pn
+;;
+
+let rec intFromBinary b =
+    match b with
+    | [] -> 0
+    | d::ds ->
+        (if d then 1 else 0) + 2 * (intFromBinary ds)
+;;
+
+let rec intFromChurch c =
+    match c with
+    | X (_) -> 0
+    | F (f) -> 1 + (intFromChurch (f ()))
+;;
+
+let rec addPeano m n =
+    match m with
+    | Z -> n
+    | S(m1) -> addPeano m1 (S (n))
+;;
+
+let rec addBinary m n =
+    let rec subAddBinary x y cIn =
+        let a = if x = [] then [false] else x
+        and b = if y = [] then [false] else y
+        in
+        if x = [] && y = [] then
+            [cIn]
+        else
+            match a, b with
+            | x0::xs, y0::ys ->
+                let sum = x0 <> y0 <> cIn
+                and cOut = (x0 && y0) || (x0 && cIn) || (y0 && cIn)
+                in
+                sum :: (subAddBinary xs ys cOut)
+            | _, _ -> []
     in
-        while !jRow < (Array.length m) do
-            jCol := !jRow + 1;
-            while !jCol < Array.length (Array.get m 0) do
-                tempR := Array.get (Array.get m !jRow) !jCol;
-                Array.set (Array.get m !jRow) !jCol (
-                    Array.get (Array.get m !jCol) !jRow
-                );
-                Array.set (Array.get m !jCol) !jRow (
-                    !tempR
-                );
-                jCol := !jCol + 1;
-            done;
-            jRow := !jRow + 1;
-        done;
+    subAddBinary m n false
+;;
+
+let rec addChurch m n =
+    match m with
+    | X(_) -> n
+    | F(f) -> addChurch (f ()) (F(fun () -> n))
 ;;
